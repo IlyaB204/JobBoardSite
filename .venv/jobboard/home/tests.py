f42731f29117172a2ff8c98e597d711f0819b928
IBobrovskiy <96880947+IBobrovskiy@users.noreply.github.com>
2025-03-13 14:14:26 +0300

    Commit today

--- .venv/jobboard/home/tests.py
+++ .venv/jobboard/home/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+
+    def test_home_view_status_code(self):
+        response = self.client.get(reverse('home'))
+        no_response = self.client.get('jobboard2/indexes.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertTemplateUsed(response, 'jobboard2/index.html')
+
+
+    def test_blog_view_status_code(self):
+        response = self.client.get(reverse('blog'))
+        no_response = self.client.get('jobboard2/bloges.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertTemplateUsed(response, 'jobboard2/blog.html')
--- .venv/jobboard/home/views.py
+++ .venv/jobboard/home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home_view(request):
 
     return render(request, 'jobboard2/index.html')
+
+
+def blog_view(request):
+    return render(request, 'jobboard2/blog.html')
